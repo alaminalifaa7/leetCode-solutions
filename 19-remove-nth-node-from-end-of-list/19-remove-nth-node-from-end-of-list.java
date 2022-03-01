ebbda89126401be2650d428e58f1ebd0fe9fe343
Alif <35551404+alaminalifaa7@users.noreply.github.com>
2022-03-01 20:38:43 +0600

    Time: 0 ms (100.00%), Space: 39.9 MB (50.74%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -11,45 +11,49 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         // not have to reverse solution
-        ListNode dummy = new ListNode(0,head);
-        ListNode left = dummy;
-        ListNode right = head;
+//         ListNode dummy = new ListNode(0,head);
+//         ListNode left = dummy;
+//         ListNode right = head;
         
-        while(n > 0 && right != null){
-            right = right.next;
-            n -=1;
-        }
-        while(right !=null){
-            left = left.next;
-            right = right.next;
-        }
-        left.next = left.next.next;
-        return dummy.next;
-      
-//         ListNode firstReversedHead = reverse(head);
-//         ListNode prev = null;
-//         ListNode current = firstReversedHead;
-//         for(int i = 1; i <= n; i++){
-//             ListNode changeLink = current;
-//             if(i = 1){
-//                firstReversedHead. 
-//             }
-            
-//             current = current.next;
+//         while(n > 0 && right != null){
+//             right = right.next;
+//             n -=1;
 //         }
-//         ListNode secHead = reverse(firstReversedHead);
-//      return   secHead;
+//         while(right !=null){
+//             left = left.next;
+//             right = right.next;
+//         }
+//         left.next = left.next.next;
+//         return dummy.next;
+      
+        ListNode firstReversedHead = reverse(head);
+        ListNode prev = new ListNode(0,firstReversedHead);
+        ListNode current = firstReversedHead;
+        for(int i = 1; i <= n; i++){
+           if(n == 1){
+              firstReversedHead = current.next;
+               break;
+           }
+            else if(i == n && i > 1){
+                prev.next = current.next;
+                break;
+            }
+            prev = prev.next;
+            current = current.next;
+        }
+        ListNode secHead = reverse(firstReversedHead);
+     return   secHead;
         
-    // }
-     // private ListNode reverse(ListNode h){
-     //        ListNode prev = null;
-     //        ListNode current = h;
-     //        while(current != null){
-     //            ListNode temp = current.next;
-     //            current.next = prev;
-     //            prev = current;
-     //            current = temp;
-     //        }
-     //        return prev;
+    }
+     private ListNode reverse(ListNode h){
+            ListNode prev = null;
+            ListNode current = h;
+            while(current != null){
+                ListNode temp = current.next;
+                current.next = prev;
+                prev = current;
+                current = temp;
+            }
+            return prev;
         }
 }
