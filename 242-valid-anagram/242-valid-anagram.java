9256ada866c6d35d8df39a7b042ad4595e735324
Alif <35551404+alaminalifaa7@users.noreply.github.com>
2022-03-04 20:31:10 +0600

    Time: 4 ms (79.75%), Space: 45 MB (20.23%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,35 +1,48 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
-        return false;
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-        HashMap<Character,Integer> map1 = new HashMap<Character,Integer>();
-        char[] ch=s.toCharArray(); 
-        char[] ch1=t.toCharArray(); 
-        for(int i=0;i<ch.length;i++){
-            if(!map.containsKey(ch[i])){
-                 map.put(ch[i], 1);
-            }else{
-                map.put(ch[i], map.get(ch[i]) + 1);
-            }
+        if(s.length()!=t.length()){
+            return false;
+        }
         
-        }
-        for(int i=0;i<ch1.length;i++){
-          if (map.containsKey(ch1[i])) {
+//         HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+//         HashMap<Character,Integer> map1 = new HashMap<Character,Integer>();
+//         char[] ch=s.toCharArray(); 
+//         char[] ch1=t.toCharArray(); 
+//         for(int i=0;i<ch.length;i++){
+//             if(!map.containsKey(ch[i])){
+//                  map.put(ch[i], 1);
+//             }else{
+//                 map.put(ch[i], map.get(ch[i]) + 1);
+//             }
+        
+//         }
+//         for(int i=0;i<ch1.length;i++){
+//           if (map.containsKey(ch1[i])) {
            
-                map.put(ch1[i],
-                        map.get(ch1[i]) - 1);
-            }
+//                 map.put(ch1[i],
+//                         map.get(ch1[i]) - 1);
+//             }
+//         }
+//          Set<Character> keys = map.keySet();
+//         // Loop over all keys and check if all keys are 0.
+//         // If so it means it is anagram.
+//         for (Character key : keys) {
+//             if (map.get(key) != 0) {
+//                 return false;
+//             }
+//         }
+//         // Returning True as all keys are zero
+//         return true;
+        
+        char[] tempArray = s.toCharArray();
+        char[] tempArray1 = t.toCharArray();
+ 
+        // Sorting temp array using
+        Arrays.sort(tempArray);
+        Arrays.sort(tempArray1);
+        if(Arrays.equals(tempArray,tempArray1)){
+            return true;
         }
-         Set<Character> keys = map.keySet();
-        // Loop over all keys and check if all keys are 0.
-        // If so it means it is anagram.
-        for (Character key : keys) {
-            if (map.get(key) != 0) {
-                return false;
-            }
-        }
-        // Returning True as all keys are zero
-        return true;
+        return false;
     }
 }
