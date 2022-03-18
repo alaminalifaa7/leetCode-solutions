d5880dd0611afebc9d4fe47041037bc4cb6af3f3
Alif <35551404+alaminalifaa7@users.noreply.github.com>
2022-03-18 10:07:04 +0600

    Time: 1 ms (67.95%), Space: 43.2 MB (9.03%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-//         int l = 0;
+        //         int l = 0;
 //         int r = nums.length - 1;
 //         //finding the pivot point/index of the smallest number -- left will hold the index of it
 //             while(l < r){
@@ -36,54 +35,54 @@ class Solution {
 //         }
 //         return -1;
         
-//         int l = 0;
-//         int r = nums.length - 1;
+        int l = 0;
+        int r = nums.length - 1;
         
-//         while(l <= r){
-//             int mid = l + (r - l)/2;
-//             if(target  == nums[mid]){
-//                 return mid;
-//             }
-            
-//             if(nums[l] <= nums[mid]){
-//                 if(target > nums[mid] || target < nums[r]){
-//                     l = mid + 1;
-//                 }else{
-//                     r = mid - 1;
-//                 }
-//             }else{
-//                 if(target < nums[mid] || target > nums[r]){
-//                     r = mid - 1;
-//                 }else{
-//                     l = mid + 1;
-//                 }
-//             }
-            
-//         }
-        
-//            return -1; 
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end){
-            int mid = (start + end) / 2;
-            if (nums[mid] == target)
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(target  == nums[mid]){
                 return mid;
-        
-            if (nums[start] <= nums[mid]){
-                 if (target < nums[mid] && target >= nums[start]) 
-                    end = mid - 1;
-                 else
-                    start = mid + 1;
-            } 
-        
-            if (nums[mid] <= nums[end]){
-                if (target > nums[mid] && target <= nums[end])
-                    start = mid + 1;
-                 else
-                    end = mid - 1;
             }
+            
+            if(nums[l] <= nums[mid]){
+                if(target > nums[mid] || target < nums[l]){
+                    l = mid + 1;
+                }else{
+                    r = mid - 1;
+                }
+            }else{
+                if(target < nums[mid] || target > nums[r]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }
+            
         }
-        return -1;
         
+           return -1; 
+//         int start = 0;
+//         int end = nums.length - 1;
+//         while (start <= end){
+//             int mid = (start + end) / 2;
+//             if (nums[mid] == target)
+//                 return mid;
+        
+//             if (nums[start] <= nums[mid]){
+//                  if (target < nums[mid] && target >= nums[start]) 
+//                     end = mid - 1;
+//                  else
+//                     start = mid + 1;
+//             } 
+        
+//             if (nums[mid] <= nums[end]){
+//                 if (target > nums[mid] && target <= nums[end])
+//                     start = mid + 1;
+//                  else
+//                     end = mid - 1;
+//             }
+//         }
+//         return -1;
+
     }
 }
